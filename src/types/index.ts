b0311bdb38f5a209c3a58f7e689cde1880f7a8b9
TONY T C NGUYEN <tnguyen0428@gmail.com>
2026-04-23 08:07:25 -0700

    Add breakStartTime to Manicurist interface
    Added breakStartTime to Manicurist interface.

--- src/types/index.ts
+++ src/types/index.ts
@@ -21,6 +21,7 @@ export interface Manicurist {
   skills: string[];
   clockedIn: boolean;
   clockInTime: number | null;
+    breakStartTime: number | null;
   totalTurns: number;
   currentClient: string | null;
   status: ManicuristStatus;
